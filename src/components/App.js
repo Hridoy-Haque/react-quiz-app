8830544a83ac34b1579594d310f0fe04701ea283
Hridoy <iamnirob334@gmail.com>
2022-07-17 10:49:38 +0600

    lesson 4 source code push

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,11 @@
+import "../Styles/App.css";
+import Layout from "./Layout";
+import Home from "./pages/Home";
+
 export default function App() {
   return (
-    <div className="App">
-      <div>Hello World</div>
-    </div>
+    <Layout>
+      <Home />
+    </Layout>
   );
 }
