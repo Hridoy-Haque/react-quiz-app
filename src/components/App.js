d503d2e92ab4646df4e16f5b5bb49ab298bf0dd8
Hridoy <iamnirob334@gmail.com>
2022-07-18 00:27:03 +0600

    lesson-8 source code upload

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,23 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "../Styles/App.css";
 import Layout from "./Layout";
-// import Home from "./pages/Home";
-// import Signup from "./pages/Signup";
-// import Login from "./pages/Login";
-// import Quiz from "./pages/Quiz";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Quiz from "./pages/Quiz";
 import Result from "./pages/Result";
-
+import Signup from "./pages/Signup";
 export default function App() {
   return (
-    <Layout>
-      {/* <Home /> */}
-      {/* <Signup /> */}
-      {/* <Login /> */}
-      {/* <Quiz /> */}
-      <Result />
-    </Layout>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/signup" element={<Signup />}></Route>
+          <Route exact path="/login" element={<Login />}></Route>
+          <Route exact path="/quiz" element={<Quiz />}></Route>
+          <Route exact path="/result" element={<Result />}></Route>
+        </Routes>
+      </Layout>
+    </Router>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,16 @@
+import { Link } from "react-router-dom";
 import logo from "../Assets/images/logo-bg.png";
 import classes from "../Styles/Nav.module.css";
 import Account from "./Account";
-
 export default function Nav() {
   return (
     <nav className={classes.nav}>
       <ul>
         <li>
-          <a href="index.html" className={classes.brand}>
+          <Link to="/" className={classes.brand}>
             <img src={logo} alt="Learn with Sumit Logo" />
             <h3>Learn with Sumit</h3>
-          </a>
+          </Link>
         </li>
       </ul>
       <Account />
